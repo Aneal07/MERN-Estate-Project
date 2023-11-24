64994a45ce40195616684fd7222cfecf1ca5c583
Aneal07 <anilmaharjan07@outlook.com>
2023-11-24 14:30:02 +0000

    add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -37,3 +37,14 @@ try {
     next(error)
 }
 }
+
+export const deleteUser = async(req, res, next ) => {
+  if(req.user.id != req.params.id) ;
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.clearCookie('access_token')
+    res.status(200).json('User has been deleted')
+  } catch (error) {
+    next(error)
+  }
+}
