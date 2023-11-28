cffffd6472bab324034420e5d53ce971f17084eb
Aneal07 <anilmaharjan07@outlook.com>
2023-11-28 10:01:58 +0000

    fixed the bug on listing images functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,51 +1,56 @@
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import React, { useState } from 'react'
 import { app } from '../firebase';
+import { set } from 'mongoose';
 
 const CreateListing = () => {
-  const [ files, setFiles ] =  useState([])
-  const [ formData, setFormData ] =useState({
+   const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
     imageUrls: [],
-  })
-  const [imageUploadError, setImageUploadError ] = useState(false);
-  const  [uploading, setUploading ] = useState(false);
-  console.log(formData)
+  });
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
+  console.log(formData);
   const handleImageSubmit = (e) => {
-    if(files.length > 0 && files.length + formData.imageUrls.length < 7) {
+    if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
       setUploading(true);
       setImageUploadError(false);
-      const  promises = [];
+      const promises = [];
 
-      for ( let i = 0; i < files.length; i++) {
+      for (let i = 0; i < files.length; i++) {
         promises.push(storeImage(files[i]));
-
       }
-      Promise.all(promises).then((urls) => {
-        setFormData({...formData, imageUrls: formData.imageUrls.concat(urls) });
-        setImageUploadError(false);
-        setUploading(false);
-      }).catch((err) => {
-        setImageUploadError('Image upload failed (2mb max per image)')
-        setUploading(false);
-      });
-    }else {
-      setImageUploadError('You can only upload 6 images per listing')
-      setUploading(false)
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData({
+            ...formData,
+            imageUrls: formData.imageUrls.concat(urls),
+          });
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        .catch((err) => {
+          setImageUploadError('Image upload failed (2 mb max per image)');
+          setUploading(false);
+        });
+    } else {
+      setImageUploadError('You can only upload 6 images per listing');
+      setUploading(false);
     }
-  }
+  };
 
   const storeImage = async (file) => {
     return new Promise((resolve, reject) => {
       const storage = getStorage(app);
       const fileName = new Date().getTime() + file.name;
-      const StorageRef = ref(storage, fileName);
-      const uploadTask = uploadBytesResumable(StorageRef, files);
-      uploadTask.on (
-        "state_changed",
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        'state_changed',
         (snapshot) => {
           const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-          console.log(`Upload is ${progress}% done`)
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
         },
         (error) => {
           reject(error);
@@ -55,16 +60,16 @@ const CreateListing = () => {
             resolve(downloadURL);
           });
         }
-      )
-    })
-  }
+      );
+    });
+  };
 
   const handleRemoveImage = (index) => {
     setFormData({
       ...formData,
       imageUrls: formData.imageUrls.filter((_, i) => i !== index),
     });
-  }
+  };
 
   return (
      <main className='p-3 max-w-4xl mx-auto'>
