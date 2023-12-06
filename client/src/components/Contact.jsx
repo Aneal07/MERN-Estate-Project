5d0ba840a55349d476729cd2c821349f48fbd920
Aneal07 <anilmaharjan07@outlook.com>
2023-12-06 16:02:53 +0000

    contact page styling

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -41,7 +41,7 @@ export default function Contact({ listing }) {
 
           <Link
           to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
-          className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
+          className='bg-red-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-90'
           >
             Send Message          
           </Link>
