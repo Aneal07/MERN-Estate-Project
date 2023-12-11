96598d54272eb541cc5154cea9a4e80c3b4afa42
Aneal07 <anilmaharjan07@outlook.com>
2023-12-11 14:51:25 +0000

    complete home page

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -60,8 +60,8 @@ const Signin = () => {
      </form>
      <div className='flex gap-2 mt-5'>
       <p>Dont Have an account?</p>
-      <Link to={"/sign-in"}>
-        <span className='text-blue-600'>Sign up</span>
+      <Link to={"/sign-up"}>
+        <span className='text-blue-600 hover:underline'>Sign up</span>
       </Link>
      </div>
      {error && <p className='text-red-500 mt-5'>{error}</p>}
